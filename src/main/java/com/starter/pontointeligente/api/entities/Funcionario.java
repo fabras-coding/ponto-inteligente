06b70dad56ec116fee4ee673cdab4bf30da24d26
Fabricio de Paula Oliveira <Fabricio_paula@hotmail.com>
2020-10-31 15:10:05 -0300

    Finalização das Entidades de BD

--- src/main/java/com/starter/pontointeligente/api/entities/Funcionario.java
+++ src/main/java/com/starter/pontointeligente/api/entities/Funcionario.java
@@ -1,5 +1,207 @@
 package com.starter.pontointeligente.api.entities;
 
-public class Funcionario {
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.*;
+
+import javax.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import com.starter.pontointeligente.api.enums.PerfilEnum;
+
+@Entity
+@Table(name = "TB_FUNCIONARIO")
+public class Funcionario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String nome;
+	private String email;
+	private String senha;
+	private String cpf;
+	private BigDecimal valorHora;
+	private Float qdtHorasTrabalhoDia;
+	private Float qdtHorasAlmoco;
+	private PerfilEnum perfil;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	private Empresa empresa;
+	private List<Lancamento> lancamentos;
+	
+	public Funcionario() {
+		
+	}
+	
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "nome", nullable=false)
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	@Column(name = "email", nullable =false)
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	@Column(name="senha", nullable = false) 
+	public String getSenha()
+	{
+		return	senha; 
+	}
+	  
+	public void setSenha(String senha) 
+	{ 
+		this.senha = senha; 
+	}
+	 
+	
+	@Column(name= "cpf", nullable = false)
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	@Column(name = "valor_hora", nullable = true)
+	public BigDecimal getValorHora() {
+		return valorHora;
+	}
+	
+	@Transient
+	public Optional<BigDecimal> getValorHoraOpt(){
+		return Optional.ofNullable(valorHora);
+	}
+	
+	public void setValorHora (BigDecimal valorHora) {
+		this.valorHora = valorHora;
+	}
+
+	@Column(name="qtd_horas_trabalho_dia", nullable = true)
+	public Float getQdtHorasTrabalhoDia() {
+		return qdtHorasTrabalhoDia;
+	}
+	
+	@Transient
+	public Optional<Float> getQdtHorasTrabalhoDiaOpt() {
+		return Optional.ofNullable(qdtHorasTrabalhoDia);
+	}
+	
+
+	public void setQdtHorasTrabalhoDia(Float qdtHorasTrabalhoDia) {
+		this.qdtHorasTrabalhoDia = qdtHorasTrabalhoDia;
+	}
+
+	@Column(name="qtd_horas_almoco", nullable = true)
+	public Float getQdtHorasAlmoco() {
+		return qdtHorasAlmoco;
+	}
+
+	@Transient
+	public Optional<Float> getQdtHorasAlmocoOpt() {
+		return Optional.ofNullable(qdtHorasAlmoco);
+	}
+	
+	public void setQdtHorasAlmoco(Float qdtHorasAlmoco) {
+		this.qdtHorasAlmoco = qdtHorasAlmoco;
+	}
+	
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name="perfil", nullable = false)
+	public PerfilEnum getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(PerfilEnum perfil) {
+		this.perfil = perfil;
+	}
+
+	@Column(name="dt_criacao", nullable = false)
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Column(name="dt_atualizacao", nullable = false)
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	@OneToMany(mappedBy = "TB_FUNCIONARIO", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+
+	public void setLancamentos(List<Lancamento> lancamentos) {
+		this.lancamentos = lancamentos;
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		dataCriacao = atual;
+		dataAtualizacao = atual;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Funcionario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf
+				+ ", valorHora=" + valorHora + ", qdtHorasTrabalhoDia=" + qdtHorasTrabalhoDia + ", qdtHorasAlmoco="
+				+ qdtHorasAlmoco + ", perfil=" + perfil + ", dataCriacao=" + dataCriacao + ", dataAtualizacao="
+				+ dataAtualizacao + ", empresa=" + empresa + ", lancamentos=" + lancamentos + "]";
+	}
+
+
+	
 }
--- src/main/java/com/starter/pontointeligente/api/entities/Lancamento.java
+++ src/main/java/com/starter/pontointeligente/api/entities/Lancamento.java
@@ -1,5 +1,130 @@
 package com.starter.pontointeligente.api.entities;
 
-public class Lancamento {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.*;
+import com.starter.pontointeligente.api.enums.TipoEnum;
+
+@Entity
+@Table(name = "TB_LANCAMENTO")
+public class Lancamento implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5754350560527888846L;
+
+	private Long id;
+	private Date dataLancamento;
+	private String descricao;
+	private String localizacao;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	private TipoEnum tipo;
+	private Funcionario funcionario;
+	
+	public Lancamento() {
+		
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "DT_LANCAMENTO", nullable = false)
+	public Date getData() {
+		return dataLancamento;
+	}
+
+	public void setData(Date dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+	@Column(name="NM_DESCRICAO", nullable = true)
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Column(name="DESC_LOCALIZACAO", nullable = true)
+	public String getLocalizacao() {
+		return localizacao;
+	}
+
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	@Column(name="DT_CRIACAO", nullable = false)
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Column(name="DT_ATUALIZACAO", nullable = false)
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	@Enumerated(EnumType.STRING)
+	@Column(name="NM_TIPO", nullable =false)
+	public TipoEnum getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoEnum tipo) {
+		this.tipo = tipo;
+	}
+
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		dataCriacao = atual;
+		dataAtualizacao = atual;
+	}
+	
+	@Override
+	public String toString() {
+		return "Lancamento [id=" + id + ", dataLancamento=" + dataLancamento + ", descricao=" + descricao + ", localizacao=" + localizacao
+				+ ", dataCriacao=" + dataCriacao + ", dataAtualizacao=" + dataAtualizacao + ", tipo=" + tipo
+				+ ", funcionario=" + funcionario + "]";
+	}
+	
+	
+	
+	
 }
