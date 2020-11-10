fdd68b0c2081eb4c0763e82310652f2707f549b6
Fabricio de Paula Oliveira <Fabricio_paula@hotmail.com>
2020-11-09 21:32:27 -0300

    Tentando resolver problema do flyway

---
+++ src/test/java/com/starter/pontointeligente/api/repositories/EmpresaRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.starter.pontointeligente.api.repositories;
+
+
+//import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class EmpresaRepositoryTest {
+
+}
