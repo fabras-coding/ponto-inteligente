60f0e71eb113cd973ee862ae23cc258f63c68bdc
Fabricio de Paula Oliveira <Fabricio_paula@hotmail.com>
2020-11-09 22:13:14 -0300

    Criando o Teste do Repositorio de Empresa

--- src/test/java/com/starter/pontointeligente/api/repositories/EmpresaRepositoryTest.java
+++ src/test/java/com/starter/pontointeligente/api/repositories/EmpresaRepositoryTest.java
@@ -1,18 +1,51 @@
 package com.starter.pontointeligente.api.repositories;
 
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//import static org.junit.Assert.assertEquals;
+import com.starter.pontointeligente.api.entities.Empresa;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class EmpresaRepositoryTest {
+	
+	
+	@Autowired //injeção de dependencia
+	private IEmpresaRepository empresaRepository;
 
+	public static final String CNPJ = "51479301000180";
+	
+	@Before //executa antes do teste
+	public void setUp() throws Exception{
+		
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de Exemplo");
+		empresa.setCnpj("01123456789");
+		this.empresaRepository.save(empresa);
+	}
+	
+	@After //...
+	public final void tearDown() {
+		this.empresaRepository.deleteAll();
+	}
+	
+	@Test
+	public void testBuscarPorCnpj() {
+		Empresa empresa = this.empresaRepository.findByCnpj(CNPJ);
+		assertEquals(CNPJ,empresa.getCnpj());
+	}
+	
+	
 }
