df0e7c64713d63b6fc7095e027be74e933915dbb
Fabricio de Paula Oliveira <Fabricio_paula@hotmail.com>
2020-11-17 22:30:09 -0300

    Arrumando Assert do JUnit

--- src/test/java/com/starter/pontointeligente/api/repositories/EmpresaRepositoryTest.java
+++ src/test/java/com/starter/pontointeligente/api/repositories/EmpresaRepositoryTest.java
@@ -3,18 +3,17 @@ package com.starter.pontointeligente.api.repositories;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.starter.pontointeligente.api.entities.Empresa;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.After;
-import org.junit.Before;
 
 
 @SpringBootTest
