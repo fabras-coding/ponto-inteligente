3fea115aa2a77e0ea9f53882cbafa9deff165668
libelola <fabriciod08@gmail.com>
2021-03-27 15:51:29 -0300

    Readicionando FuncionarioRepository com correção da dependencia

--- src/test/java/com/starter/pontointeligente/api/repositories/FuncionarioRepositoryTest.java
+++ src/test/java/com/starter/pontointeligente/api/repositories/FuncionarioRepositoryTest.java
@@ -1,23 +1,23 @@
 package com.starter.pontointeligente.api.repositories;
 
-//
-//import org.junit.jupiter.api.AfterEach;
-////import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-//
-//import com.starter.pontointeligente.api.entities.Empresa;
-//import com.starter.pontointeligente.api.entities.Funcionario;
-//import com.starter.pontointeligente.api.enums.PerfilEnum;
-//import com.starter.pontointeligente.api.utils.PasswordUtils;
-//
-//import static org.junit.Assert.assertNotNull;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import java.security.NoSuchAlgorithmException;
+
+import com.starter.pontointeligente.api.entities.Empresa;
+import com.starter.pontointeligente.api.entities.Funcionario;
+import com.starter.pontointeligente.api.enums.PerfilEnum;
+import com.starter.pontointeligente.api.utils.PasswordUtils;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.security.NoSuchAlgorithmException;
 
 
 
@@ -25,89 +25,89 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 public class FuncionarioRepositoryTest {
 	
-//	@Autowired
-//	private IFuncionarioRepository funcionarioRepository;
-//	
-//	@Autowired
-//	private IEmpresaRepository empresaRepository;
-//	
-//	private static final String EMAIL = "email@email.com";
-//	private static final String CPF = "24514587845";
-//	
-//	@BeforeEach
-//	public void setUp() throws Exception {
-//		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
-//		this.funcionarioRepository.save(obterDadosFuncionario(empresa));
-//		
-//	}
-//	
-//	
-//
-//	@AfterEach
-//	public final void tearDown() {
-//		this.empresaRepository.deleteAll();
-//	}
-//		
-//	@Test
-//	public void testBuscarFuncionarioPorEmail() {
-//		Funcionario funcionario = this.funcionarioRepository.findByEmail(EMAIL);
-//		assertEquals(EMAIL, funcionario.getEmail());
-//	}
-//	
-//	
-//	@Test
-//	public void testBuscarFuncionarioPorCpf() {
-//		Funcionario funcionario = this.funcionarioRepository.findByCpf(CPF);
-//		assertEquals(CPF, funcionario.getCpf());
-//	}
-//	
-//	
-//
-//	@Test
-//	public void testBuscarFuncionarioPorEmailECpf() {
-//		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail(CPF, EMAIL);
-//		assertNotNull(funcionario);
-//	}
-//	
-//	
-//
-//	@Test
-//	public void testBuscarFuncionarioPorEmailOuCpfParaEmailInvalido() {
-//		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail(CPF, "email@invalido.com");
-//		assertNotNull(funcionario);
-//	}
-//	
-//
-//	@Test
-//	public void testBuscarFuncionarioPorEmailOuCpfParaCpfInvalido() {
-//		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail("12345678901",EMAIL);
-//		assertNotNull(funcionario);
-//	}
-//	
-//	
-//	private Funcionario obterDadosFuncionario(Empresa empresa) throws NoSuchAlgorithmException{
-//		Funcionario funcionario =  new Funcionario();
-//		funcionario.setNome("Fulano de Tal");
-//		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
-//		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456"));
-//		funcionario.setCpf(CPF);
-//		funcionario.setEmail(EMAIL);
-//		funcionario.setEmpresa(empresa);
-//		
-//		return funcionario;
-//		
-//		
-//	}
-//	
-//	private Empresa obterDadosEmpresa() {
-//
-//		Empresa empresa = new Empresa();
-//		empresa.setCnpj("123456789000121");
-//		empresa.setRazaoSocial("Empresa dos Testes");
-//		
-//		return empresa;
-//
-//	}
+	@Autowired
+	private IFuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	private IEmpresaRepository empresaRepository;
+	
+	private static final String EMAIL = "email@email.com";
+	private static final String CPF = "24514587845";
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
+		this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+		
+	}
+	
+	
+
+	@AfterEach
+	public final void tearDown() {
+		this.empresaRepository.deleteAll();
+	}
+		
+	@Test
+	public void testBuscarFuncionarioPorEmail() {
+		Funcionario funcionario = this.funcionarioRepository.findByEmail(EMAIL);
+		assertEquals(EMAIL, funcionario.getEmail());
+	}
+	
+	
+	@Test
+	public void testBuscarFuncionarioPorCpf() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpf(CPF);
+		assertEquals(CPF, funcionario.getCpf());
+	}
+	
+	
+
+	@Test
+	public void testBuscarFuncionarioPorEmailECpf() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail(CPF, EMAIL);
+		assertNotNull(funcionario);
+	}
+	
+	
+
+	@Test
+	public void testBuscarFuncionarioPorEmailOuCpfParaEmailInvalido() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail(CPF, "email@invalido.com");
+		assertNotNull(funcionario);
+	}
+	
+
+	@Test
+	public void testBuscarFuncionarioPorEmailOuCpfParaCpfInvalido() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail("12345678901",EMAIL);
+		assertNotNull(funcionario);
+	}
+	
+	
+	private Funcionario obterDadosFuncionario(Empresa empresa) throws NoSuchAlgorithmException{
+		Funcionario funcionario =  new Funcionario();
+		funcionario.setNome("Fulano de Tal");
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456"));
+		funcionario.setCpf(CPF);
+		funcionario.setEmail(EMAIL);
+		funcionario.setEmpresa(empresa);
+		
+		return funcionario;
+		
+		
+	}
+	
+	private Empresa obterDadosEmpresa() {
+
+		Empresa empresa = new Empresa();
+		empresa.setCnpj("123456789000121");
+		empresa.setRazaoSocial("Empresa dos Testes");
+		
+		return empresa;
+
+	}
 
 }
 	
